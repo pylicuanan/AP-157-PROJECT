ad5b457929b8407fa9c953f0d6a1300471e2b0e4
dabidyyy <dpmagpusao@up.edu.ph>
2025-05-16 15:12:39 +0800

    Added # of coins vs accuracy data

--- CoinFaceDetector.py
+++ CoinFaceDetector.py
@@ -1,5 +1,6 @@
 ## automatic 10 secs
 import cv2 as cv
+import pandas as pd
 import numpy as np
 import datetime
 import time
@@ -7,13 +8,15 @@ import TemplateMatching
 
 tails_file = 'Tails.jpg'
 heads_file = 'Heads.jpg'
-flag = cv.IMREAD_UNCHANGED
+flag = cv.IMREAD_COLOR_BGR
 method = cv.TM_CCOEFF_NORMED
-tails_threshold = 0.55
-heads_threshold = 0.45
+tails_threshold = 0.4
+heads_threshold = 0.6
 
 # Initialize camera
 cap = cv.VideoCapture(1)
+cap.set(cv.CAP_PROP_EXPOSURE,-4)
+
 
 # Set video codec and output settings
 fourcc = cv.VideoWriter_fourcc(*'XVID')
@@ -22,7 +25,7 @@ recording = False
 auto_capture = False
 last_capture_time = 0
 image_counter = 0  # Counter for number of auto-captured images
-capture_interval = 5  # seconds
+capture_interval = 10  # seconds
 
 num_heads = 0
 num_tails = 0
@@ -30,6 +33,13 @@ num_tails = 0
 print(f"Press 'c' to start automatic image capture every {capture_interval} seconds")
 print("Press 'r' to reset counter")
 print("Press 'q' to quit")
+
+data = {
+    "capture_count": image_counter,
+    "num_heads": num_heads,
+    "num_tails": num_tails
+}
+df = pd.DataFrame([data])
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -62,18 +72,27 @@ while cap.isOpened():
                                                          heads_threshold, tails_threshold)
             num_heads += result.num_heads
             num_tails += result.num_tails
+            
+            if result.num_heads!=0 or result.num_tails!=0:
+                new_data = {
+                    "capture_count": image_counter,
+                    "num_heads": result.num_heads,
+                    "num_tails": result.num_tails
+                }
+                df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+            
         else:
             print('No image')
-        
+
 
     # Overlay image counter
     overlay_text = f"Images Captured: {image_counter}"
     cv.putText(frame, overlay_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX,
                 1, (0, 255, 0), 2, cv.LINE_AA)
     cv.putText(frame, f"Heads: {num_heads}", (480, 30), cv.FONT_HERSHEY_SIMPLEX,
-                1, (0,0,0), 2, cv.LINE_AA)
+                1, (255,255,0), 2, cv.LINE_AA)
     cv.putText(frame, f"Tails: {num_tails}", (480, 70), cv.FONT_HERSHEY_SIMPLEX,
-                1, (0,0,0), 2, cv.LINE_AA)
+                1, (255,255,0), 2, cv.LINE_AA)
 
     # Overlay countdown timer
     if auto_capture:
@@ -95,6 +114,11 @@ while cap.isOpened():
         else:
             auto_capture = False
             print("Automatic capture stopped.")
+            
+            if df.shape[0]>1:
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                data_filename = f"datasets/data_{timestamp}.csv"
+                df.to_csv(data_filename, index=False)
 
     elif key == ord('r'):
         num_heads = 0
@@ -103,6 +127,11 @@ while cap.isOpened():
         if auto_capture:
             auto_capture = False
             print("Automatic capture stopped.")
+        
+        if df.shape[0]>1:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            data_filename = f"datasets/data_{timestamp}.csv"
+            df.to_csv(data_filename, index=False)
             
 
     elif key == ord('q'):
